cdddc519d0bdf293ea9cf9c7cddbb97e6f0510f9
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2024-04-08 16:51:27 -0600

    changes on index.js

--- index.js
+++ index.js
@@ -11,19 +11,21 @@ function hanldeForm(){
             fetch("http://localhost:3000/dogBreeds") // fetching animal data from local server
             .then(response => response.json()) // taking the response from the local server and converting it into a json file - Java Script Object Notation
             .then(data => {  // taking the data from the converted JSON file itterating 
-               const tableBody = document.getElementById("data-body")
-               console.log(tableBody)
-               tableBody.innerHTML = "" // clears the previous results
+               
+        
+                const tableBody = document.getElementById("data-body") // grabbing the ID data-body form the DOM
+                console.log(tableBody) //console logging to make sure it was grabbed
+                tableBody.innerHTML = "" // clears the previous results
 
-                data.forEach(breed => {
-                    const row = document.createElement("tr")
+                data.forEach(breed => { // taking the data, that was converted into a JSON, and using a forEach loop to itterate through the array of objects)
+                    const row = document.createElement("tr") // creating a new element called row so that it can be appending to the tableBody element - this is where all of the appended elements will be stored
                 
                 //create and append the dog name cell
-                    const nameCell = document.createElement("td")
-                    nameCell.textContent = breed.DogName
-                    row.appendChild(nameCell)
+                    const nameCell = document.createElement("td") // creating a new element that holds the table data (td)
+                    nameCell.textContent = breed.DogName // changing the text of the created element to the data that we have in our array of objects and using dot notation(chain method) to acccess the DogName index
+                    row.appendChild(nameCell) // appending the nameCell element, that was just created, to the row element that we created, within our forLoop
 
-                    const descriptionCell = document.createElement("td")
+                    const descriptionCell = document.createElement("td") 
                     descriptionCell.textContent = breed.description
                     row.appendChild(descriptionCell)
 
@@ -32,21 +34,17 @@ function hanldeForm(){
                     row.appendChild(typeCell)
 
                     tableBody.appendChild(row)
+
+                //Input
+                
+                
                 })
                 
             })
      })
  }
 
-        hanldeForm()
-
-document.addEventListener("click",() =>
- hanldeForm)
-
- console.log("hello")
-
-
-        
+hanldeForm()
 
 
 
