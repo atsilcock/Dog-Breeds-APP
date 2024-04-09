cab668659896d3ff5c8fe8ece9df9b7189a7104d
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2024-04-09 12:26:31 -0600

    4-9-24

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("form"); // Grabbing the form element from the index.html
+const form = document.getElementById("form"); 
 
 document.addEventListener("DOMContentLoaded", () => {
     fetchInformation();
@@ -8,30 +8,48 @@ document.addEventListener("DOMContentLoaded", () => {
     function resetForm ()  {
         const resetButton = document.getElementById("reset-button");
         console.log(resetButton);
-        resetButton.addEventListener("click", (e) => { // Passing a click event listener as the parameter
-            e.preventDefault(); // This event is preventing the default action of the button - which is to submit/reset
+        resetButton.addEventListener("click", (e) => { 
+            e.preventDefault(); 
             form.reset()
             
         });
     }
 
 
+    function commentForm(){
+        const textForm = document.getElementById("text")
+        console.log(textForm)
+        textForm.addEventListener("keydown", (e) => {
+            console.log(e)
+            if(e.key === "Enter"){
+                //e.preventDefault()
+                alert("Your message has been recieved!")
+            }
+        })
+        
+    }
+    
+    commentForm()
+          
+
+
+
 function fetchInformation() {
-    fetch("http://localhost:3000/dogBreeds") // Fetching animal data from local server
-    .then(response => response.json()) // Taking the response from the local server and converting it into a json file - JavaScript Object Notation
+    fetch("http://localhost:3000/dogBreeds") 
+    .then(response => response.json()) 
     .then(data => {
         data.forEach(breed => {
             const tableBody = document.getElementById("data-table");
-            const row = document.createElement("tr"); // Creating a new element called row
+            const row = document.createElement("tr"); 
             
-            //Click event listener - this is to click on element which takes the row and clones it to the table body, while at removing the elements in the table
+            
             row.addEventListener("click", () => {
                 row.style.color = "red";
                 tableBody.innerHTML =  ""
                 tableBody.appendChild(row.cloneNode(true))
                 
 
-            // On the event listener - click, this is meant to show the image of that specific breed    
+             
                 const postImage = document.getElementById("picture-body")
                 console.log(postImage)
                 const animalImage = document.createElement("img")
@@ -58,5 +76,6 @@ function fetchInformation() {
         });
     });
 }
+
 
 
